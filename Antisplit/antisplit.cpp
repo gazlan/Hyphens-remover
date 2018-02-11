1f1250193133e4675888e6e238a751a35d790dc1
* <user@aol.com>
2018-02-12 02:28:07 +0500

    Initial release

---
+++ Antisplit/antisplit.cpp
@@ -0,0 +1,191 @@
+/* ******************************************************************** **
+** @@ Antisplit Hyphens remover
+** @  Copyrt : 
+** @  Author : 
+** @  Update :
+** @  Update :
+** @  Notes  :
+** ******************************************************************** */
+
+/* ******************************************************************** **
+**                uses pre-compiled headers
+** ******************************************************************** */
+
+#include "stdafx.h"
+
+#include "..\shared\file_find.h"
+
+/* ******************************************************************** **
+** @@                   internal defines
+** ******************************************************************** */
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+#ifdef NDEBUG
+#pragma optimize("gsy",on)
+#pragma comment(linker,"/FILEALIGN:512 /MERGE:.rdata=.text /MERGE:.data=.text /SECTION:.text,EWR /IGNORE:4078")
+#endif 
+
+/* ******************************************************************** **
+** @@                   internal prototypes
+** ******************************************************************** */
+
+/* ******************************************************************** **
+** @@                   external global variables
+** ******************************************************************** */
+
+/* ******************************************************************** **
+** @@                   static global variables
+** ******************************************************************** */
+
+static bool    bRemoveTabs       = true;
+static bool    bRemoveEmptyLines = false;
+
+/* ******************************************************************** **
+** @@                   real code
+** ******************************************************************** */
+
+/* ******************************************************************** **
+** @@ Proceed()
+** @  Copyrt : 
+** @  Author : 
+** @  Modify :
+** @  Update :
+** @  Notes  :
+** ******************************************************************** */
+
+static void Proceed(const char * const   pszFileName)
+{
+   char     pszBackName[_MAX_PATH];
+   char     pszDrive   [_MAX_DRIVE];
+   char     pszDir     [_MAX_DIR];
+   char     pszFName   [_MAX_FNAME];
+   char     pszExt     [_MAX_EXT];
+
+   _splitpath(pszFileName,pszDrive,pszDir,pszFName,pszExt);
+   _makepath( pszBackName,pszDrive,pszDir,pszFName,"BAK");
+
+   CopyFile(pszFileName,pszBackName,FALSE);
+
+   FILE*    pIn = fopen(pszBackName,"rb");
+
+   if (!pIn)
+   {
+      perror("\a\nOpen Input File Error !\n");
+      return;
+   }
+
+   FILE*    pOut = fopen(pszFileName,"wb");
+
+   if (!pOut)
+   {
+      perror("\a\nOpen Output File Error !\n");
+      fclose(pIn);
+      pIn = NULL;
+      return;
+   }
+
+   int   Chr = 0;
+
+   while ((Chr = fgetc(pIn)) != EOF)
+   {
+      if (Chr != 0x2D)
+      {
+         fputc(Chr,pOut);
+      }
+      else
+      {
+         int   Chr2 = fgetc(pIn);
+
+         if (Chr2 != 0x0D)
+         {
+            fputc(Chr, pOut);
+            fputc(Chr2,pOut);
+         }
+         else
+         {
+            int   Chr3 = fgetc(pIn);
+
+            if (Chr3 != 0x0A)
+            {
+               fputc(Chr, pOut);
+               fputc(Chr2,pOut);
+               fputc(Chr3,pOut);
+            }
+         }
+      }
+   }
+
+   fclose(pIn);
+   pIn = NULL;
+
+   fclose(pOut);
+   pOut = NULL;
+}
+
+/* ******************************************************************** **
+** @@ ShowHelp()
+** @  Copyrt : 
+** @  Author : 
+** @  Modify :
+** @  Update :
+** @  Notes  :
+** ******************************************************************** */
+
+static void ShowHelp()
+{
+   const char  pszCopyright[] = "-*-   Antisplit   *   (c)  Gazlan,  2013   -*-";
+   const char  pszDescript [] = "Anti-Hyphenation. Hyphens remover";
+   const char  pszE_Mail   [] = "complains_n_suggestions direct to gazlan@yandex.ru";
+
+   printf("%s\n\n",pszCopyright);
+   printf("%s\n\n",pszDescript);
+   printf("Usage: asp.com wildcards\n\n");
+   printf("%s\n\n",pszE_Mail);
+}
+
+/* ******************************************************************** **
+** @@ main()
+** @  Copyrt : 
+** @  Author : 
+** @  Modify :
+** @  Update :
+** @  Notes  :
+** ******************************************************************** */
+
+int main(int argc,char** argv)
+{
+   if (argc != 2)
+   {
+      ShowHelp();
+      return 0;
+   }
+
+   if (argc == 2 && ((!strcmp(argv[1],"?")) || (!strcmp(argv[1],"/?")) || (!strcmp(argv[1],"-?")) || (!stricmp(argv[1],"/h")) || (!stricmp(argv[1],"-h"))))
+   {
+      ShowHelp();
+      return 0;
+   }
+
+   FindFile   FF;
+
+   FF.SetMask(argv[1]);
+
+   while (FF.Fetch())
+   {
+      if ((FF._w32fd.dwFileAttributes | FILE_ATTRIBUTE_NORMAL) && !(FF._w32fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+      {
+         Proceed(FF._w32fd.cFileName);
+      }
+   }
+
+   return 0;
+}
+
+/* ******************************************************************** **
+** @@                   The End
+** ******************************************************************** */
